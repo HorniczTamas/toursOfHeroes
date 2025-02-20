d025edb2cccaca7c1a5e46e0ab27a73ea9983556
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-02-20 09:26:43 +0100

    asd

--- src/app/_components/hero-detail/hero-detail.component.ts
+++ src/app/_components/hero-detail/hero-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, input, Input, OnInit } from '@angular/core';
+import { Component, inject, input, Input, OnInit } from '@angular/core';
 import { Hero } from '../../_models/hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../../_services/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,7 +13,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class HeroDetailComponent implements OnInit{
   hero = input<Hero>()
-
+  route = inject(ActivatedRoute)
+  heroSErvice = inject(HeroService)
   // @Input() hero? : Hero
   selectedHero? : Hero
 
--- src/app/_components/heroes/heroes.component.ts
+++ src/app/_components/heroes/heroes.component.ts
@@ -1,36 +1,24 @@
-import { HEROES } from './../../_models/mock-heroes';
-import { UpperCasePipe } from '@angular/common';
 import { Hero } from './../../_models/hero';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../../_services/hero.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { MessageService } from '../../_services/message.service';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
-  imports: [UpperCasePipe, FormsModule, HeroDetailComponent, HttpClientModule, RouterModule],
+  imports: [FormsModule, HttpClientModule, RouterModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent implements OnInit{
   heroService = inject(HeroService)
-  // messageService = inject(MessageService)
-  selectedHero?: Hero;
   heroList: Hero[] = [];
 
   // Ez egy interface
   ngOnInit(): void {
     this.getHeroes()
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    // this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void{
     this.heroService.getHeroes().subscribe({
       next: res => this.heroList = res,
